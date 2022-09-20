949090a330a504124c13ac83f16414a2d51dda90
sirdav1d <ddavid.diniz@gmail.com>
2022-09-19 23:16:17 -0300

    feat(schema): gender

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Moon Gaming')
+    .setDescription('Aplicação para gestão de plataforma de jogos')
+    .setVersion('2.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
